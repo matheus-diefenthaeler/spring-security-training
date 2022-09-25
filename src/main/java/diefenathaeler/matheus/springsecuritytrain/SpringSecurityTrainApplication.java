8fe250f55a0b27ccfc5152f8d7e02cd30ad8b43c
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-09-24 21:20:15 -0300

    Adding project

---
+++ src/main/java/diefenathaeler/matheus/springsecuritytrain/SpringSecurityTrainApplication.java
@@ -0,0 +1,13 @@
+package diefenathaeler.matheus.springsecuritytrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/diefenathaeler/matheus/springsecuritytrain/SpringSecurityTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package diefenathaeler.matheus.springsecuritytrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
